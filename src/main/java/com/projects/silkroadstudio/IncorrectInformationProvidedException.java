8478b391414a00cf83fc353b74cb0f3ab36b1f8f
guillevazquez8 <guillevazquez8@gmail.com>
2022-12-10 17:53:41 +0100

    #1 User class finished!

---
+++ src/main/java/com/projects/silkroadstudio/IncorrectInformationProvidedException.java
@@ -0,0 +1,9 @@
+package com.projects.silkroadstudio;
+
+public class IncorrectInformationProvidedException extends Exception {
+
+    public IncorrectInformationProvidedException(String message) {
+        super(message);
+    }
+
+}
--- src/main/java/com/projects/silkroadstudio/User.java
+++ src/main/java/com/projects/silkroadstudio/User.java
@@ -1,8 +1,59 @@
 package com.projects.silkroadstudio;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@NoArgsConstructor
+@Getter
+@Inheritance(strategy = InheritanceType.JOINED)
 public class User {
 
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+
+    private String name;
+
+    private String surname;
+
+    private String email;
+
+    private String password;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setEmail(String email) throws IncorrectInformationProvidedException {
+        if (!email.contains("@")) {
+            throw new IncorrectInformationProvidedException("Email provided is incorrect, please provide the right email");
+        }
+        this.email = email;
+    }
+
+    public void setPassword(String password) throws IncorrectInformationProvidedException {
+        if (password.length() < 10) {
+            throw new IncorrectInformationProvidedException("Your password is too short");
+        }
+        this.password = password;
+    }
+
+    public User(String name, String surname, String email, String password) throws IncorrectInformationProvidedException {
+        this.name = name;
+        this.surname = surname;
+        setEmail(email);
+        setPassword(password);
+    }
+
+    public User(String email, String password) throws IncorrectInformationProvidedException {
+        setEmail(email);
+        setPassword(password);
+    }
 }
